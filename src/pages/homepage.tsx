d6a07aa83612f8f8d22b68d4df133c0f174801cf
Yusuf Bektaş <agolho@gmail.com>
2024-05-02 21:06:02 +0300

    added toasts

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -5,6 +5,10 @@ const Homepage = () => {
         <div className={"main-content-inside"}>
             {/* Add your Connect Wallet page content here */}
             <video src={"/video/lofi.mp4"} autoPlay muted></video>
+            <div className={"homepage-titles"}>
+                <h1>Stray Cat Tribe</h1>
+                <h3>Game HUB</h3>
+            </div>
         </div>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from "next/head";
 import { Nav, Container } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
-import { Inter } from 'next/dist/compiled/@next/font/dist/google'
+import {enqueueSnackbar, SnackbarProvider} from 'notistack/';
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 config.autoAddCss = false
 
 import SpeedyPaws from "@/pages/speedypaws";
@@ -39,9 +38,8 @@ export default function Home() {
 
 	const [allowedWallets, setAllowedWallets] = useState<string[]>([]);
 	const [isAllowed, setIsAllowed] = useState(false);
+	const [toastShown, setToastShown] = useState(false);
 	const { publicKey } = useWallet();
-
-
 
 	useEffect(() => {
 		const allowedWalletsString = process.env.ALLOWED_WALLETS;
@@ -53,9 +51,12 @@ export default function Home() {
 		}
 	}, []);
 
-
 	useEffect(() => {
 		setIsAllowed(allowedWallets.includes(publicKey?.toBase58() || ""));
+		if (publicKey && isAllowed && !toastShown) {
+			enqueueSnackbar('Welcome to SCT Premium', { variant: "success", anchorOrigin: { vertical: "bottom", horizontal: "right" } });
+			setToastShown(true);
+		}
 	}, [publicKey, allowedWallets]);
 
 	const [unityInstance, setUnityInstance] = useState<any>(null);
@@ -108,6 +109,10 @@ export default function Home() {
 
 	return (
 		<>
+			<SnackbarProvider
+				Components={{
+
+			}} />
 			<style jsx global>{`
 				@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 				html {
