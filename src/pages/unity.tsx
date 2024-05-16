2215e2f4a40fb3b4ea46265068015fe071fb55da
Yusuf Bektaş <agolho@gmail.com>
2024-05-16 03:58:44 +0300

    unity update

--- src/pages/unity.tsx
+++ src/pages/unity.tsx
@@ -86,7 +86,7 @@ const UnityComponent: React.FC<UnityProps> = ({ gameName, dataUrl, frameworkUrl,
             </Button>
             </div>
             <div ref={unityContainerRef} id="unity-container" className="unity-desktop">
-                <canvas id="unity-canvas" width="1280" height="720"></canvas>
+                <canvas id="unity-canvas" width="960" height="600"></canvas>
                 <div id="unity-loading-bar">
                     <div id="unity-logo">
                         <img src={"/TemplateData/unity-logo-dark.png"}></img>
